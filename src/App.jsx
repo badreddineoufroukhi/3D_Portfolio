53198b786aca2273e6af2fcaba12e463d802c4ce
Badr eddine oufroukhi <badreddineoufroukhi2@gmail.com>
2025-11-27 14:04:36 +0100

    Ticket 1 : Init Project / Setup Git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>welcome to our portfolio!</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
